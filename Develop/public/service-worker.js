b82f3bccb04784970d55c8de98ff17e43eb4c186
whosshazo <ryandeshazo1@gmail.com>
2022-04-09 10:25:26 -0400

    start of service worker.js

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = "budget-app-";
+const VERSION = "v1";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
+
+const FILES_TO_CACHE = [
+  "/",
+  "./index.html",
+  "./css/styles.css",
+  "./index.js",
+  "./manifest.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+];
