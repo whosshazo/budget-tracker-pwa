6a45bd3597b7959a2287dca24368d9e8109d98e8
whosshazo <ryandeshazo1@gmail.com>
2022-04-09 21:39:04 -0400

    service worker activation

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -7,6 +7,7 @@ const FILES_TO_CACHE = [
   "/",
   "./index.html",
   "./css/styles.css",
+  // "./js/idb.js",
   "./index.js",
   "./manifest.js",
   "./icons/icon-72x72.png",
@@ -18,3 +19,55 @@ const FILES_TO_CACHE = [
   "./icons/icon-384x384.png",
   "./icons/icon-512x512.png",
 ];
+
+//fetch
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  );
+});
+
+//install Service Worker
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+//activate service worker
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeepList = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeepList.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeepList.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
